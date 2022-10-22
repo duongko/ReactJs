4f1db36e454ae26c9e211f74d2059e92667cff94
duongko <pow3401@gmail.com>
2022-10-23 02:39:42 +0700

    video3/setState

--- src/App.js
+++ src/App.js
@@ -9,11 +9,24 @@ class App extends React.Component {
     name: 'dương',
     age: '23',
   }
+
+  handelclick(event) {
+    // console.log('cảm ơn bạn rất nhiều')
+    console.log(event)
+    console.log("tên tôi là :", this.state.name)
+    this.setState({
+      name: "minh anh"
+
+      ///hàm setState là hàm giúp cập nhập/thaydoi  của thằng state
+    })
+  }
   render() {
     return (
       <div>
         <h1>hello world</h1>
         my name is {this.state.name}
+
+        <button onClick={(event) => { this.handelclick(event) }}>click me</button>
       </div>
     )
   }
