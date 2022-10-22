35441f73df68b25f6e7645550ec6fb742eabf18c
duongko <pow3401@gmail.com>
2022-10-23 01:54:03 +0700

    video2/state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 import Mycomponent from './components/Mycomponent';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <Mycomponent />
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    name: 'dương',
+    age: '23',
+  }
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+        my name is {this.state.name}
+      </div>
+    )
+  }
+
+
+
+
+
+
+
+
+
 }
 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//           <Mycomponent />
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 export default App;
