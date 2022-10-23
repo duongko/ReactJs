cf974079001605e051b7916439b61a21a41a8f32
duongko <pow3401@gmail.com>
2022-10-23 19:59:11 +0700

    5/show data array/obj

--- src/coponent/Displayinfo.js
+++ src/coponent/Displayinfo.js
@@ -2,18 +2,31 @@ import React from "react";
 
 class Displayinfo extends React.Component {
 
+
     render() {
         //prop: property truyền dữ liệu từ component cha sang component con
         console.log(this.props)
-        const { age, name } = this.props;
+        const { listUser } = this.props;
 
         //đây là cách viết destructuring <=> const age= this.props.age
         return (
 
             <div>
-                tên của tôi là :{name}
+                {/* tên của tôi là :{name}
                 <br></br>
-                tuổi: {age}
+                tuổi: {age} */}
+
+                {listUser.map((user) => {
+                    return (
+                        <div key={user.id}>
+                            tên của tôi là :{user.name}
+                            <br></br>
+                            tuổi: {user.age}
+                            <hr></hr>
+
+                        </div>
+                    )
+                })}
             </div>
 
         )
--- src/coponent/Mycomponent.js
+++ src/coponent/Mycomponent.js
@@ -3,7 +3,15 @@ import Userinfo from './Userinfo';
 import Displayinfo from './Displayinfo';
 
 class Mycomponent extends React.Component {
+    state = {
+        listUser: [
+            { id: 1, name: "hỏi dân it", age: '22' },
+            { id: 2, name: "dương", age: '28' },
+            { id: 3, name: "minh anh", age: '22' },
+        ]
 
+
+    }
 
 
 
@@ -13,7 +21,12 @@ class Mycomponent extends React.Component {
             <div>
                 <Userinfo />
                 <hr />
-                <Displayinfo name="hỏi dân it" age="12" />
+                <Displayinfo
+                    listUser={this.state.listUser}
+                    User={this.state.listUser}
+
+
+                />
 
 
 
