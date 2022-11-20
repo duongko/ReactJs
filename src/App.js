42d3086ef6474f71be68260cb54a4eb8cf05b71a
duongko <pow3401@gmail.com>
2022-11-20 23:49:22 +0700

    redux

--- src/App.js
+++ src/App.js
@@ -2,27 +2,37 @@ import logo from './logo.svg';
 import './App.scss';
 import Mycomponent from './coponent/Mycomponent';
 import React from 'react';
+import { decreaseCounter, increaseCounter } from './coponent/redux/action/counterAction';
+import { useDispatch, useSelector } from 'react-redux'
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
+const App = () => {
+  const count = useSelector(state => state.counter.count)
+  const dispatch = useDispatch()
 
-        <h3>hello tất cả các bạn</h3>
-        <Mycomponent />
-      </>
+  return (
+    <>
 
-    )
-  }
+      <h3>hello tất cả các bạn</h3>
+      <div>
 
+        <span>count={count} </span>
+        <button onClick={() => dispatch(increaseCounter())}>tăng</button>
+        <span> </span>
+        <button onClick={() => dispatch(decreaseCounter())}>giảm</button>
+      </div>
+    </>
 
-
-
-
-
-
-
+  )
 }
+
+
+
+
+
+
+
+
+
 
 
 // function App() {
